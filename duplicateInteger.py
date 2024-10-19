be6cdaf8c5e0d4e03d63f9c277e98536b4d2b23c
Berra Koç <144827481+berrakoc@users.noreply.github.com>
2024-10-19 22:57:23 +0300

    updated

--- duplicateInteger.py
+++ duplicateInteger.py
@@ -1,3 +1,6 @@
+"""
+#time complexity: O(n^2) this approach is inefficient, slow for large inputs
+
 from typing import List
 
 class Solution:
@@ -26,3 +29,35 @@ print(solution.hasDuplicate(nums3))  # Output: False
 # Test case 4: List with one element
 nums4 = [1]
 print(solution.hasDuplicate(nums4))  # Output: False
+"""
+#this one is way faster, O(n)
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+
+# Test
+solution = Solution()
+
+# Test case 1: List with duplicates
+nums1 = [1, 2, 3, 4, 5, 2]
+print(solution.containsDuplicate(nums1))  # Output: True
+
+# Test case 2: List without duplicates
+nums2 = [1, 2, 3, 4, 5]
+print(solution.containsDuplicate(nums2))  # Output: False
+
+# Test case 3: Empty list
+nums3 = []
+print(solution.containsDuplicate(nums3))  # Output: False
+
+# Test case 4: List with one element
+nums4 = [1]
+print(solution.containsDuplicate(nums4))  # Output: False
